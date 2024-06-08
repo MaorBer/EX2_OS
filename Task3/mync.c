388f9d59085ceeae19c3eb2b292d139dd9665d56
DanielBespalov <156921697+DanielBespalov@users.noreply.github.com>
2024-06-08 19:05:14 +0300

    Update mync.c

--- Task3/mync.c
+++ Task3/mync.c
@@ -14,8 +14,8 @@ void print_usage(const char *prog_name)
     fprintf(stderr, "Usage: %s -e <command> [-i <input>] [-o <output>] [-b <both>]\n", prog_name);
 }
 
-void handle_tcp_server(int port) {
-    int server_socket;
+int s_socket_tcp(int port){
+    int server_socket; 
     struct sockaddr_in server_address;
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,24 +23,36 @@ void handle_tcp_server(int port) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
+    int yes = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0)
+    {
+        perror("setsockopt() failed");
+    }
 
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(port);
-
+ 
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
         perror("bind");
         exit(EXIT_FAILURE);
     }
+    
+    return server_socket;
+}
+
+void handle_tcp_server(int port, int port2, char opt, char opt2, char** args) {
+    int server_socket = s_socket_tcp(port);
+    int server_socket2;
+    int client_socket2, client_socket;
+
+    struct sockaddr_in client_address;
+    socklen_t client_address_length = sizeof(client_address);
 
     if (listen(server_socket, 5) == -1) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-
-    int client_socket;
-    struct sockaddr_in client_address;
-    socklen_t client_address_length = sizeof(client_address);
 
     client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_length);
     if (client_socket == -1) {
@@ -48,10 +60,60 @@ void handle_tcp_server(int port) {
         exit(EXIT_FAILURE);
     }
 
-    // Redirect child process's stdout to client socket
-    dup2(client_socket, STDOUT_FILENO);
+    if (port2 != 0) {
+        server_socket2 = s_socket_tcp(port2);
+        if (listen(server_socket2, 1) == -1) {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
 
-    close(server_socket); // Close the server socket, as it's no longer needed
+        struct sockaddr_in client_address2;
+        socklen_t client_address_length2 = sizeof(client_address2);
+
+        client_socket2 = accept(server_socket2, (struct sockaddr *)&client_address2, &client_address_length2);
+        if (client_socket2 == -1) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+    }
+
+    if (opt == 'i' && opt2 == ' '){
+        dup2(client_socket, STDIN_FILENO);
+        execv(args[0], args);
+    }
+
+    else if(opt == 'o' && opt2 == ' '){
+        dup2(client_socket, STDOUT_FILENO);
+        execv(args[0], args);
+    }
+
+    else if(opt == 'b' && opt2 == ' '){
+        dup2(client_socket, STDIN_FILENO);
+        dup2(client_socket, STDOUT_FILENO);
+        execv(args[0], args);
+    }
+
+    else if(opt == 'i' && opt2 == 'o')
+    {
+        dup2(client_socket, STDIN_FILENO);
+        dup2(client_socket2, STDOUT_FILENO);
+        execv(args[0], args);
+    }
+
+    else if(opt == 'o' && opt2 == 'i')
+    {
+        dup2(client_socket2, STDIN_FILENO);
+        dup2(client_socket, STDOUT_FILENO);
+        execv(args[0], args);
+    }
+
+
+    close(server_socket);
+    close(client_socket);
+    close(server_socket2);
+    close(client_socket2);
+
 }
 
 
@@ -93,6 +155,9 @@ int main(int argc, char *argv[])
     char *output = NULL;
     int port = 0;
     char *host = NULL;
+    int server_socket;
+    int both = 0;
+    int port2 = 0;
 
     // Parse command-line arguments
     while ((opt = getopt(argc, argv, "e:i:o:b:")) != -1)
@@ -109,8 +174,10 @@ int main(int argc, char *argv[])
             output = optarg;
             break;
         case 'b':
+            both = 1;
             input = optarg;
             output = optarg;
+
             break;
         default:
             print_usage(argv[0]);
@@ -118,11 +185,11 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (command == NULL || (input == NULL && output == NULL))
+    /*if (command == NULL || (input == NULL && output == NULL))
     {
         print_usage(argv[0]);
         return EXIT_FAILURE;
-    }
+    }*/
 
     // Create a child process
     pid_t pid = fork();
@@ -135,68 +202,49 @@ int main(int argc, char *argv[])
 
     if (pid == 0)
     {
+        char *args[100]; // Assuming we won't have more than 100 arguments
+        int arg_count = 0;
+
+        char *token = strtok(command, " ");
+        while (token != NULL){
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[arg_count] = NULL;
         // Child process
-        if (input != NULL && strncmp(input, "TCPS", 4) == 0)
+        if(output == NULL && input == NULL)
+            execv(args[0], args);
+  
+        else if (output == NULL && strncmp(input, "TCPS", 4) == 0) //TCPS AND I 
         {
-            // Handle TCP server input
             port = atoi(input + 4);
-            handle_tcp_server(port);
+            handle_tcp_server(port, 0, 'i', ' ', args);
         }
-        else if (output != NULL && strncmp(output, "TCPC", 4) == 0)
+
+        else if(output != NULL && input != NULL && strncmp(input, "TCPS", 4) == 0 && strncmp(output, "TCPS", 4) == 0)
         {
-            // Handle TCP client output
-            char *delim = strchr(output + 4, ',');
-            if (delim == NULL)
-            {
-                print_usage(argv[0]);
-                return EXIT_FAILURE;
-            }
-            *delim = '\0';
-            host = output + 4;
-            port = atoi(delim + 1);
-            handle_tcp_client(host, port);
-        }
-        else if (input != NULL && output != NULL && strncmp(input, "TCPS", 4) == 0 && strncmp(output, "TCPC", 4) == 0)
-        {
-            // Handle both TCP server and client
             port = atoi(input + 4);
-            handle_tcp_server(port);
-            char *delim = strchr(output + 4, ',');
-            if (delim == NULL)
-            {
-                print_usage(argv[0]);
-                return EXIT_FAILURE;
-            }
-            *delim = '\0';
-            host = output + 4;
-            port = atoi(delim + 1);
-            handle_tcp_client(host, port);
+            port2 = atoi(output + 4);
+            handle_tcp_server(port, port2, 'i', 'o', args);
         }
+
+        else if(input == NULL && strncmp(output, "TCPS", 4) == 0)
+        {
+            port = atoi(output + 4);
+            handle_tcp_server(port, 0, 'o', ' ', args);
+        }
+        
+        else if(strncmp(input, "TCPS", 4) == 0 && both)
+        {
+            port = atoi(input + 4);
+            handle_tcp_server(port, 0, 'b', ' ', args);
+        }
+
         else
         {
             print_usage(argv[0]);
             return EXIT_FAILURE;
         }
-
-        char *args[3] = {"ttt", "123456789", NULL};
-        execv(args[0], args);
-
-        // // Execute the command
-        // char *
-        //     args[100]; // Assuming we won't have more than 100 arguments
-        // int arg_count = 0;
-
-        // char *token = strtok(command, " ");
-        // while (token != NULL)
-        // {
-        //     args[arg_count++] = token;
-        //     token = strtok(NULL, " ");
-        // }
-        // args[arg_count] = NULL; // Null-terminate the array
-
-        // execv(args[0], args);
-        perror("execv");
-        return EXIT_FAILURE;
     }
     else
     {
