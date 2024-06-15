5087794e8d28c34c0c0ae5ed2a30f52bf7c8b4b1
Maor <maorw9@gmail.com>
2024-06-15 17:18:10 +0300

    declaration .h change

--- mync.c
+++ mync.c
@@ -12,13 +12,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/un.h>
+#include "mync.h"
 
-#define SOCKET_PATH "/tmp/unix_socket_example"
-#define BUFFER_SIZE 1024
 
-char *socket_path;
-int udp_socket;
-volatile sig_atomic_t time_up = 0;
 
 void uds_client_datagram(char *socket_path, char **args)
 {
