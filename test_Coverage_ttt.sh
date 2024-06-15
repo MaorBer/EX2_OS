235b277def201346bf85f10587936e4182da900d
Maor <maorw9@gmail.com>
2024-06-15 21:28:14 +0300

    clean

--- test_Coverage_ttt.sh
+++ test_Coverage_ttt.sh
@@ -10,7 +10,7 @@ gcov ttt.c
 
 # Test case 1: Testing a valid input
 echo "Test case 1: Valid input"
-./ttt 123456789 > output1.txt << EOF
+./ttt 123456789 
 1
 2
 5
@@ -20,14 +20,14 @@ diff -s output1.txt expected_output1.txt
 
 # Test case 2: Testing an invalid input
 echo "Test case 2: Invalid input"
-./ttt 987654321 > output2.txt << EOF
+./ttt 987654321 
 $
 EOF
 diff -s output2.txt expected_output2.txt
 
 # Test case 3: Testing edge case 1
 echo "Test case 3: Edge case 1"
-./ttt 12312312/111 > output3.txt << EOF
+./ttt 12312312/111 
 3
 6
 9
@@ -36,7 +36,7 @@ diff -s output3.txt expected_output3.txt
 
 # Test case 4: Testing edge case 2
 echo "Test case 4: Edge case 2"
-./ttt 321321@# > output4.txt << EOF
+./ttt 321321@# 
 1
 2
 3
@@ -45,7 +45,7 @@ diff -s output4.txt expected_output4.txt
 
 # Test case 5: Testing performance
 echo "Test case 5: Performance"
-time ./ttt 123456789 > output5.txt << EOF
+time ./ttt 123456789 
 1
 2
 3
